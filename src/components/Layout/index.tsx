3159838a8b6908a1c646ec9521b104836ef66c4b
cdfortes <cdfortes@gmail.com>
2021-10-04 13:01:17 -0100

    typewriter

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,7 +2,13 @@ import React, { useState } from 'react'
 
 import Header from '../Header'
 
+import { useTypewriter } from 'react-simple-typewriter'
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { text } = useTypewriter({
+    words: ['Hello', 'From', 'Typewri', 'Hook!'],
+    loop: true // Infinit
+  })
   return (
     <div className="mi-wrapper">
       <Header />
@@ -12,7 +18,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             <div className="mx-auto col-lg-9 col-xl-9 col-md-10">
               <div className="text-center hero-content">
                 <h1 className="mb-30 wow fadeInUp" data-wow-delay=".2s">
-                  Lorem ipsum, dolor sit amet
+                  Lorem ipsum, dolor sit amet {text}
                 </h1>
                 <p className="wow fadeInUp" data-wow-delay=".4s">
                   Lorem, ipsum dolor sit amet consectetur adipisicing elit.
